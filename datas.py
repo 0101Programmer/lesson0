caef660378d4abd86790bbb98c3fa8af547929c3
0101Programmer <baranov-v-k@yandex.ru>
2024-07-20 20:21:27 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,13 @@
+print('Hello') # comment
+print(type('Hello')) # string - строка, integer - целое число, float - дробные
+# ** возведение в степень
+# != не равно
+print(type(True), type(False)) # boolean
+print(5 == 0, 5 == 5)
+print(1, 'Hi', 1+1, False, 1 != 5)
+print(6 != 0 and 10 > 4)
+print(6 == 8 or 8 < 16)
+
+print('5')
+print(type(int('5')))
+print(type(str('5')))
